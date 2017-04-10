913978c92a3243b12494bffe7b32121b22882bf7
Ryan Thissen <rmt1855@gmail.com>
2017-04-10 14:41:13 -0700

    started sign in form, added redux-form

--- picnic/src/reducers/root_reducer.js
+++ picnic/src/reducers/root_reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import SessionReducer from './session_reducer';
+
 
 const RootReducer = combineReducers({
   session: SessionReducer,
+  form: formReducer,
 });
 
 export default RootReducer;
