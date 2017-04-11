e7aea2ebc828943917976202155f4494751ca662
Thomas Stang <stang.tk@gmail.com>
2017-04-11 09:22:00 -0700

    fixed merge conflicts

--- picnic/src/actions/index.js
+++ picnic/src/actions/index.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+import axios from 'axios';
+
 const SELECT_SEARCH_TYPE = 'SELECT_SEARCH_TYPE';
 
 const selectSearchType = (selection) => {
@@ -14,29 +15,26 @@ const selectCheeseFirmness = selection => ({
   selection,
 });
 
-export { SELECT_SEARCH_TYPE, selectSearchType, selectCheeseFirmness };
-=======
-import axios from 'axios';
-
 function fetchCheese(query) {
   const API_URL = 'http://cheeswhiz.herokuapp.com/api/cheese';
 
   return axios
-    .get(API_URL)
-    .then(response => response.data);
+  .get(API_URL)
+  .then(response => response.data);
 }
 
-export const login = props =>
-  // console.log('login props', props);
-   ({
-     type: '@@redux-form/SET_SUBMIT_SUCCEEDED',
-     payload: this.props, //payload will be result of API call
-   });
+const login = props =>
+// console.log('login props', props);
+({
+  type: '@@redux-form/SET_SUBMIT_SUCCEEDED',
+  payload: this.props, //payload will be result of API call
+});
 
-export const getCheese = (query) => {
+const getCheese = (query) => {
   return {
     type: 'GET_CHEESE',
     payload: fetchCheese(query),
-  }
+  };
 };
->>>>>>> 5637a11a68960700a09e2c07e8287a4c7880d882
+
+export { SELECT_SEARCH_TYPE, selectSearchType, selectCheeseFirmness, login, getCheese };
